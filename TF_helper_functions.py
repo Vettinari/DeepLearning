894b4612889854aed30d2c755213480433b3b1d8
Miłosz Bertman <milosz.bertman@gmail.com>
2021-12-27 21:36:09 +0100

    Update TF_helper_functions.py

--- TF_helper_functions.py
+++ TF_helper_functions.py
@@ -26,7 +26,7 @@ def dataset_to_numpy(dataset, batched = False, only_labels = False):
   else:
     return data_out, labels_out
 
-def load_and_prep_image(filename, img_shape = 224, scale=True):
+def load_and_prep_image(filename, img_shape = (224,224), scale=True):
   """
   Reads in an image from filename, turns it into a tensor and reshapes into
   (224, 224, 3).
@@ -41,7 +41,7 @@ def load_and_prep_image(filename, img_shape = 224, scale=True):
   # Decode it into a tensor
   img = tf.image.decode_jpeg(img)
   # Resize the image
-  img = tf.image.resize(img, [img_shape, img_shape])
+  img = tf.image.resize(img, img_shape)
   if scale:
     # Rescale the image (get all values between 0 and 1)
     return img/255.
